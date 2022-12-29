71e6b7d625c610630e10d433c9489de32844d7cc
Vladimir Pasev <vladimir@fridgella.com>
2022-12-30 00:37:57 +0200

    depl'

---
+++ src/core/header/locationpin.jsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const SvgComponent = (props) => (
+  <img src="locationpin.svg" alt="pin" style={{width: '20px', color: "red"}}/>
+)
+
+export default SvgComponent
---
+++ src/core/header/locationpingrun.jsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const SvgComponentgrun = (props) => (
+  <img src="locationpingrun.svg" alt="pin" style={{width: '20px', color: "red"}}/>
+)
+
+export default SvgComponentgrun
